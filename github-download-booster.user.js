1cd92e16236881f773a183318d2324e1e72fc98a
Hicham Bchina <106634745+Edhic1@users.noreply.github.com>
2024-05-30 23:28:53 +0100

    Update github-download-booster.user.js

--- github-download-booster.user.js
+++ github-download-booster.user.js
@@ -19,7 +19,7 @@
 // @grant        GM_notification
 // @grant        window.onurlchange
 // @sandbox      JavaScript
-// @license      GPL-3.0 License
+// @license      MIT License
 // @run-at       document-end
 // @supportURL   https://github.com/Edhic1/Github-Download-Booster
 // @homepageURL  https://github.com/Edhic1/Github-Download-Booster
